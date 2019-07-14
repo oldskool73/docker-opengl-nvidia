11cb2e6dd12c2ed44641a4c53ffb398eeaac613b
Adam <oldskool73@gmail.com>
2019-07-14 18:21:39 +1000

    Better nvidia driver file version checking

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,16 @@
 FROM nvidia/cuda:9.0-base
+ARG nvidia_version
 
-RUN apt-get update
-RUN apt-get install -y x-window-system
-RUN apt-get install -y binutils
-RUN apt-get install -y mesa-utils
-RUN apt-get install -y module-init-tools
+RUN apt-get update \
+    && apt-get install -y \
+        x-window-system \
+        binutils \
+        mesa-utils \
+        module-init-tools \
+    && apt-get clean && rm -rf /var/lib/apt/lists/*
 
-ADD nvidia-driver.run /tmp/nvidia-driver.run
-RUN sh /tmp/nvidia-driver.run -a -N -q --ui=none --no-kernel-module --install-libglvnd
-RUN rm /tmp/nvidia-driver.run
+ADD nvidia-driver.${nvidia_version}.run /tmp/nvidia-driver.run
+RUN sh /tmp/nvidia-driver.run -a -N -q --ui=none --no-kernel-module --install-libglvnd \
+    && rm /tmp/nvidia-driver.run
 
 CMD /bin/bash
--- build.sh
+++ build.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
+## This will build a docker image from the local ./Dockerfile
+## With nvidia drivers installed matching the current host version
+## And tag it with the driver version number
+
 # Get your current host nvidia driver version, e.g. 390.48
 nvidia_version=$(cat /proc/driver/nvidia/version | head -n 1 | awk '{ print $8 }')
 
 # We must use the same driver in the image as on the host
-if test ! -f nvidia-driver.run; then
+if test ! -f nvidia-driver.${nvidia_version}.run; then
   nvidia_driver_uri=http://us.download.nvidia.com/XFree86/Linux-x86_64/${nvidia_version}/NVIDIA-Linux-x86_64-${nvidia_version}.run
-  wget -O nvidia-driver.run $nvidia_driver_uri
+  wget -O nvidia-driver.${nvidia_version}.run ${nvidia_driver_uri}
 fi
 
-docker build -t opengl-nvidia:${nvidia_version} .
+docker build --build-arg nvidia_version=${nvidia_version} -t opengl-nvidia:${nvidia_version} .
 docker tag opengl-nvidia:${nvidia_version} opengl-nvidia:latest
